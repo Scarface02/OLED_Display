cf869012a099d5aeced32730202d126f2a9fd528
Gulyás Péter <41050881+Potex@users.noreply.github.com>
2024-04-26 08:27:51 +0200

    Delete OLED_DISPLAY.X/build/default/production directory

--- OLED_DISPLAY.X/build/default/production/SSD1322_API.o.d
+++
@@ -1,6 +0,0 @@
-build/default/production/SSD1322_API.o: SSD1322_API.c SSD1322_HW_Driver.h \
- SSD1322_API.h
-
-SSD1322_HW_Driver.h:
-
-SSD1322_API.h:
--- OLED_DISPLAY.X/build/default/production/SSD1322_GFX.o.d
+++
@@ -1,8 +0,0 @@
-build/default/production/SSD1322_GFX.o: SSD1322_GFX.c SSD1322_API.h \
- SSD1322_HW_Driver.h SSD1322_GFX.h
-
-SSD1322_API.h:
-
-SSD1322_HW_Driver.h:
-
-SSD1322_GFX.h:
